f06e60ec9d58f2a95475468b123d0442db600ed4
shashank kumar <shashanksingh202020@gmail.com>
2024-09-16 15:12:58 +0530

    Welcome SpringServlet

---
+++ src/InterruptedException.java
@@ -0,0 +1,9 @@
+
+public record InterruptedException() {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Thread.java
@@ -0,0 +1,9 @@
+
+public class Thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
